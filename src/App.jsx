3dd09cf1afb0546f1fce40deec3ca40c7d706385
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-28 09:42:29 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
         </GoogleMapReact>
       </div> */}
-      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ex officiis quasi nemo perferendis sit, temporibus ipsa suscipit distinctio! Nemo dolor distinctio laboriosam similique? Dolore quae facilis magnam laboriosam eius officiis ut non culpa, quidem voluptatum temporibus quasi odit perferendis saepe tempora quam aliquam consequuntur eos assumenda labore incidunt molestiae nemo minus officia! Mollitia minima ullam ipsam dicta delectus. Ut possimus numquam, odit a sunt nostrum corporis similique inventore repellendus ullam veniam quidem accusamus. Voluptate dolores voluptatum rem perferendis, maiores rerum labore dolor! Est quod cumque asperiores labore officia id sint blanditiis magnam suscipit esse quia hic, quis temporibus deserunt.</h2>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ex officiis quasi nemo perferendis sit, temporibus ipsa suscipit distinctio! Nemo dolor distinctio laboriosam similique? Dolore quae facilis magnam laboriosam eius officiis ut non culpa, quidem voluptatum temporibus quasi odit perferendis saepe tempora quam aliquam consequuntur eos assumenda labore incidunt molestiae nemo minus officia! Mollitia minima ullam ipsam dicta delectus. Ut possimus numquam, odit a sunt nostrum corporis similique inventore repellendus ullam veniam quidem accusamus. Voluptate dolores voluptatum rem perferendis, maiores rerum labore dolor! Est quod cumque asperiores labore officia id sint blanditiis magnam suscipit esse quia hic,  quis temporibus deserunt.</h2>
     </>
   );
 }
