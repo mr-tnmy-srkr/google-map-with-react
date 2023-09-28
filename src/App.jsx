d44ab1f841c39980a51f8f472c4ecb697252a5de
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-28 12:59:19 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import GoogleMapReact from "google-map-react";
 import "./App.css";
+import { FaMapMarkedAlt } from 'react-icons/fa';
 import { googleAPIKey } from "./assets/googleAPIKey";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => (
+  <div style={{ color: "red",fontSize:"30px" }}>
+    <FaMapMarkedAlt></FaMapMarkedAlt>
+  </div>
+);
 
 function App() {
   const defaultProps = {
     center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
+      lat: 23.232513,
+      lng: 87.863419,
     },
-    zoom: 11,
+    zoom: 14,
   };
 
   return (
@@ -21,7 +26,12 @@ function App() {
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
         >
-          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+          {/* <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" /> */}
+          <AnyReactComponent
+            lat={defaultProps.center.lat}
+            lng={defaultProps.center.lng}
+            text="My Marker"
+          />
         </GoogleMapReact>
       </div>
     </>
