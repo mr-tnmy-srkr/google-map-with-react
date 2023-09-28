6f6eb6e25c5c9c0e130fd2f1177a594fc9a2645d
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-28 12:24:42 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <>
-      {/* <div style={{ height: "100vh", width: "100%" }}>
+      <div style={{ height: "500px", width: "500px" }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: googleAPIKey }}
           defaultCenter={defaultProps.center}
@@ -23,8 +23,7 @@ function App() {
         >
           <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
         </GoogleMapReact>
-      </div> */}
-      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque ex officiis quasi nemo perferendis sit, temporibus ipsa suscipit distinctio! Nemo dolor distinctio laboriosam similique? Dolore quae facilis magnam laboriosam eius officiis ut non culpa, quidem voluptatum temporibus quasi odit perferendis saepe tempora quam aliquam consequuntur eos assumenda labore incidunt molestiae nemo minus officia! Mollitia minima ullam ipsam dicta delectus. Ut possimus numquam, odit a sunt nostrum corporis similique inventore repellendus ullam veniam quidem accusamus. Voluptate dolores voluptatum rem perferendis, maiores rerum labore dolor! Est quod cumque asperiores labore officia id sint blanditiis magnam suscipit esse quia hic,  quis temporibus deserunt.</h2>
+      </div>
     </>
   );
 }
